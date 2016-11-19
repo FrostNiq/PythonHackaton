e6568318d013a28a765deae661fa5efe2ace1945
FrostNiq <jarda.mraz@gmail.com>
2016-11-19 15:27:07 +0100

    next level

---
+++ Turtle.py
@@ -0,0 +1,14 @@
+#Square
+
+import turtle
+
+turtle = turtle.Turtle()
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
