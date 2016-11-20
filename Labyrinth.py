ffaabda668e34ec4c9c4bfeb2f4f46ff9fec67bb
FrostNiq <jarda.mraz@gmail.com>
2016-11-20 16:17:03 +0100

    first part done

--- Labyrinth.py
+++ Labyrinth.py
@@ -1,10 +1,11 @@
-
+from pprint import pprint
+from collections import deque
 path = ""
 
 
 
 maze=[
-  [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
   [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -15,15 +16,33 @@ maze=[
   [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
   [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
   [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1]]
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+def getDictionaryOfNeighbours(maze):
+  dictionaryOfNeighbours = {}
+
+  for i in range(0, len(maze)):
+    for j in range(0, len(maze)):
+      if maze[i][j] == 0:
+        dictionaryOfNeighbours[(i,j)]= []
+
+  for i in range(0, len(maze)):
+    for j in range(0, len(maze)):
+      if maze[i][j] == 0 and maze[i][j+1] == 0:
+        dictionaryOfNeighbours[(i,j)].append((i,j+1))
+        dictionaryOfNeighbours[(i,j+1)].append((i,j))
+      if maze[i][j] == 0 and maze[i+1][j] == 0:
+        dictionaryOfNeighbours[(i,j)].append((i+1,j))
+        dictionaryOfNeighbours[(i+1,j)].append((i,j))
+
+  return dictionaryOfNeighbours
+
+def findPathInMaze(start):
+  path = ""
+  visited = []
+  planned = deque(["", start])
+
+  #overit jestli jsem tam uz nebyl nebo je tot muj cil?
 
 
-dictionaryOfNeighbours = {}
-
-for i in range(0, len(maze)):
-  for j in range(0, len(maze)):
-    if maze[i][j] == 0 and maze[i][j+1] == 0:
-      dictionaryOfNeighbours[i,j] = [i,j+1]
-
-print(dictionaryOfNeighbours)
- 
+  return path
